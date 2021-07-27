6d7f64e9a06ff07078e4a4ab858b6d565f9b3014
bharat <bisht4748@gmail.com>
2021-07-27 10:03:04 +0530

    first commit

---
+++ src/main/java/mvn/GithubProject/App.java
@@ -0,0 +1,7 @@
+package mvn.GithubProject;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/mvn/GithubProject/AppTest.java
@@ -0,0 +1,12 @@
+package mvn.GithubProject;
+
+//import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+	/*
+	 * @Test public void shouldAnswerWithTrue() { assertTrue(true); }
+	 */
+}
